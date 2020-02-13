f03bbacbfbc527841cc67de2cd2ef2ef6a79b77a
Muhammad Idrees <midexigner@gmail.com>
2020-02-13 16:32:11 +0500

    added Switch route

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import Navbar from './components/Navbar';
-import { Route, BrowserRouter } from 'react-router-dom'
+import { Route, BrowserRouter,Switch } from 'react-router-dom'
 import Home from './components/Home'
 import About from './components/About'
 import Contact from './components/Contact'
@@ -13,10 +13,12 @@ render() {
     <BrowserRouter>
     <div className="app">
       <Navbar/>
+          <Switch>
           <Route exact path='/' component={Home}/>
           <Route path='/about' component={About} />
           <Route path='/contact' component={Contact} />
           <Route path='/:post_id' component={Post} />
+          </Switch>
     </div>
     </BrowserRouter>
   );
