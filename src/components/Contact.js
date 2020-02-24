9c9b286b20fb5ed42749b371b837dd9fd020e1e2
Muhammad Idrees <midexigner@gmail.com>
2020-02-24 13:27:28 +0500

    added sendEmail.php and validation

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import axios from 'axios'
+// import axios from 'axios'
 //  https://www.youtube.com/watch?v=EPnBO8HgyRU
 
 export default class Contact extends Component {
@@ -8,15 +8,26 @@ export default class Contact extends Component {
 
     this.state = {
       fname: '',
+      fnameError: '',
       lname: '',
+      lnameError: '',
       cemail: '',
+      cemailError: '',
       currentweb: '',
+      currentwebError: '',
       budget: '',
+      budgetError: '',
       project: '',
+      projectError: '',
       interested: '',
-      details: '',
+      interestedError: '',
+      cdetails: '',
+      detailsError: '',
       about: '',
+      aboutError: '',
       referred: '',
+      referredError: '',
+      mailSent: false,
     }
 
     this.handleChange = this.handleChange.bind(this)
@@ -24,25 +35,117 @@ export default class Contact extends Component {
 
   }
 
-  handleChange = e =>{
-    this.setState({[e.target.name]:e.target.value })
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value })
   }
-handleSubmit = (e) =>{
-e.preventDefault();
-const {fname,lname,cemail,currentweb,budget,project,interested,details,about,referred} = this.state;
-const form =  axios.post('/api/form',{
-  fname,
-  lname,
-  cemail,
-  currentweb,
-  budget,
-  project,
-  interested,
-  details,
-  about,
-  referred
-});
-}
+
+  validate = () => {
+    let isError = false;
+
+    const errors = {
+      fnameError: "",
+      lnameError: "",
+      cemailError: "",
+      currentwebError: "",
+      budgetError: "",
+      projectError: "",
+      interestedError: "",
+      detailsError: "",
+      aboutError: "",
+      referredError: "",
+
+    };
+
+    if (this.state.fname === '') {
+      isError = true;
+      errors.fnameError = "Requires valid First Name";
+    }
+    if (this.state.lname === '') {
+      isError = true;
+      errors.lnameError = "Requires valid Last Name";
+    }
+    if (this.state.cemail.indexOf("@") === -1) {
+      isError = true;
+      errors.cemailError = "Requires valid email";
+    }
+    if (this.state.currentweb === '') {
+      isError = true;
+      errors.currentwebError = "Requires valid Current Web";
+    }
+    if (this.state.budget === '') {
+      isError = true;
+      errors.budgetError = "Select Budget";
+    }
+    if (this.state.project === '') {
+      isError = true;
+      errors.projectError = "Select Project";
+    }
+    if (this.state.interested === '') {
+      isError = true;
+      errors.interestedError = "Select Interested";
+    }
+
+    if (this.state.cdetails === '') {
+      isError = true;
+      errors.detailsError = "Requires valid Details";
+    }
+    if (this.state.about === '') {
+      isError = true;
+      errors.aboutError = "Select About";
+    }
+
+    if (this.state.referred === '') {
+      isError = true;
+      errors.referredError = "Requires valid referred";
+    }
+
+    this.setState({
+      ...this.state,
+      ...errors
+    });
+
+    return isError;
+
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const { fname, lname, cemail, currentweb, budget, project, interested, cdetails, about, referred } = this.state;
+    const err = this.validate();
+    if (!err) {
+      
+    this.setState(
+      {
+        fnameError: "",
+        lnameError: "",
+        cemailError: "",
+        currentwebError: "",
+        budgetError: "",
+        projectError: "",
+        interestedError: "",
+        detailsError: "",
+        aboutError: "",
+        referredError: "",
+  
+      }
+    );
+     
+      /*  const form =  axios.post('/api/form',{
+        fname,
+        lname,
+        cemail,
+        currentweb,
+        budget,
+        project,
+        interested,
+        cdetails,
+        about,
+        referred
+      });  */
+    }
+    console.log(this.state);
+    console.log('its work');
+  }
   render() {
 
     return (
@@ -55,75 +158,83 @@ const form =  axios.post('/api/form',{
                 <div className="row">
                   <div className=" col s6">
                     <label htmlFor="fname">First Name</label>
-                    <input type="text" name="fname" onChange={this.handleChange} />
+                    <input type="text" name="fname" defaultValue={this.state.fname} onChange={this.handleChange} />
+                    <span className="helper-text red-text lighten-1">{this.state.fnameError}</span>
                   </div>
                   <div className=" col s6">
                     <label htmlFor="lname">Last Name</label>
-                    <input type="text" name="lname" onChange={this.handleChange} />
+                    <input type="text" name="lname" defaultValue={this.state.lname} onChange={this.handleChange} />
+                    <span className="helper-text red-text lighten-1">{this.state.lnameError}</span>
                   </div>
 
                 </div>
                 <div className="row">
                   <div className=" col s12">
                     <label htmlFor="cemail">Email</label>
-                    <input type="text" name="cemail" className="autocomplete" onChange={this.handleChange} />
+                    <input type="text" name="cemail" defaultValue={this.state.cemail} onChange={this.handleChange} />
+                    <span className="helper-text red-text lighten-1">{this.state.cemailError}</span>
                   </div>
                 </div>
 
                 <div className="row">
                   <div className="col s12">
                     <label htmlFor="currentweb">Current website address (if applicable):</label>
-                    <input type="text" name="currentweb" onChange={this.handleChange} />
+                    <input type="text" name="currentweb" defaultValue={this.state.currentweb} onChange={this.handleChange} />
+                    <span className="helper-text red-text lighten-1">{this.state.currentwebError}</span>
                   </div>
                 </div>
                 <div className="row">
                   <div className=" col s12">
                     <label htmlFor="budget">Budget</label>
-                    <select className="browser-default" name="budget" onChange={this.handleChange}>
+                    <select className="browser-default" name="budget" defaultValue={this.state.budget || "I need ongoing design work"} onChange={this.handleChange}>
                       <option disabled="" value="" className="">Select</option>
-                      <option label="$800 – $2,000" value="$800 – $2,000">$800 – $2,000</option>
-                      <option label="$2,000 – $5,000" value="$2,000 – $5,000">$2,000 – $5,000</option>
-                      <option label="$5,000 –&nbsp;$10,000" value="$5,000 –&nbsp;$10,000">$5,000 –&nbsp;$10,000</option>
-                      <option label="$10,000+" value="$10,000+">$10,000+</option>
-                      <option label="I need ongoing design work" value="I need ongoing design work" selected="selected">I need ongoing design work</option>
+                      <option label="$800 – $2,000">$800 – $2,000</option>
+                      <option label="$2,000 – $5,000" >$2,000 – $5,000</option>
+                      <option label="$5,000 – $10,000" >$5,000 – $10,000</option>
+                      <option label="$10,000+">$10,000+</option>
+                      <option label="I need ongoing design work">I need ongoing design work</option>
 
                     </select>
+                    <span className="helper-text red-text lighten-1">{this.state.budgetError}</span>
                   </div>
                 </div>
                 <div className="row">
                   <div className=" col s12">
                     <label htmlFor="project">When are you looking to start your project?</label>
-                    <select className="browser-default" name="project" onChange={this.handleChange}>
-                      <option disabled="" value="" className="">Select</option>
-                      <option label="Right away!" value="Right away!">Right away!</option>
-                      <option label="Within the next month or so." value="Within the next month or so.">Within the next month or so.</option>
+                    <select className="browser-default" name="project" defaultValue={this.state.project || "I'm not sure."} onChange={this.handleChange}>
+                      <option disabled="" className="">Select</option>
+                      <option label="Right away!" >Right away!</option>
+                      <option label="Within the next month or so." >Within the next month or so.</option>
                       <option label="I'm in no rush, just ready to take action." value="I'm in no rush, just ready to take action.">I'm in no rush, just ready to take action.</option>
-                      <option label="I'm not sure." value="I'm not sure." selected="selected">I'm not sure.</option>
+                      <option label="I'm not sure.">I'm not sure.</option>
 
 
                     </select>
+                    <span className="helper-text red-text lighten-1">{this.state.projectError}</span>
                   </div>
                 </div>
 
                 <div className="row">
                   <div className=" col s12">
                     <label htmlFor="interested">What service are you interested in?</label>
-                    <select className="browser-default" name="interested" onChange={this.handleChange}>
-                      <option disabled="" value="" className="" selected="selected">Select</option>
-                      <option label="Web Design &amp; Development" value="Web Design &amp; Development">Web Design &amp; Development</option>
-                      <option label="Just Web Design" value="Just Web Design">Just Web Design</option>
-                      <option label="Digital Ad and/or Social Media Design" value="Digital Ad and/or Social Media Design">Digital Ad and/or Social Media Design</option>
-                      <option label="Email Design" value="Email Design">Email Design</option>
-                      <option label="Other/I'm not sure what I need" value="Other/I'm not sure what I need">Other/I'm not sure what I need</option>
+                    <select className="browser-default" name="interested" defaultValue={this.state.interested || "Select"} onChange={this.handleChange}>
+                      <option disabled="" label="Select" className="">Select</option>
+                      <option label="Web Design &amp; Development">Web Design &amp; Development</option>
+                      <option label="Just Web Design" >Just Web Design</option>
+                      <option label="Digital Ad and/or Social Media Design" >Digital Ad and/or Social Media Design</option>
+                      <option label="Email Design">Email Design</option>
+                      <option label="Other/I'm not sure what I need">Other/I'm not sure what I need</option>
 
                     </select>
+                    <span className="helper-text red-text lighten-1">{this.state.interestedError}</span>
                   </div>
                 </div>
 
                 <div className="row">
                   <div className=" col s12">
-                    <label htmlFor="details" name="details">Please provide more details about your project: *</label>
-                    <input type="text" id="details" className="autocomplete" onChange={this.handleChange} />
+                    <label htmlFor="cdetails" >Please provide more details about your project: *</label>
+                    <input type="text" name="cdetails" defaultValue={this.state.cdetails} onChange={this.handleChange} />
+                    <span className="helper-text red-text lighten-1">{this.state.detailsError}</span>
                   </div>
                 </div>
 
@@ -131,20 +242,22 @@ const form =  axios.post('/api/form',{
                 <div className="row">
                   <div className=" col s12">
                     <label htmlFor="about">How did you hear about me?*</label>
-                    <select className="browser-default" onChange={this.handleChange}>
-                      <option selected="selected" disabled="" value="">Select</option>
+                    <select className="browser-default" name="about" defaultValue={this.state.about || "Select"} onChange={this.handleChange}>
+                      <option disabled="" label="Select">Select</option>
                       <option label="My Website Google" >My Website/Google</option>
                       <option label="Social Media" >Social Media</option>
                       <option label="Referral" >Referral</option>
                       <option label="Dribbble" >Dribbble</option>
                       <option label="Other" >Other</option>
                     </select>
+                    <span className="helper-text red-text lighten-1">{this.state.aboutError}</span>
                   </div>
                 </div>
                 <div className="row">
                   <div className=" col s12">
                     <label htmlFor="referred">If you were referred to me, who sent you here?</label>
-                    <input type="text" id="about" className="autocomplete" onChange={this.handleChange} />
+                    <input type="text" name="about" defaultValue={this.state.referred} onChange={this.handleChange} />
+                    <span className="helper-text red-text lighten-1">{this.state.referredError}</span>
                   </div>
                 </div>
                 <div className="row">
