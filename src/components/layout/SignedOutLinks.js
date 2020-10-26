16850bd5c87840d302c2931eb6fe24e58669a5d9
Muhammad Idrees <midexigner@gmail.com>
2020-10-26 14:27:31 +0500

    nav hide in mobile

--- src/components/layout/SignedOutLinks.js
+++ src/components/layout/SignedOutLinks.js
@@ -3,10 +3,17 @@ import {NavLink} from 'react-router-dom'
 
 const SignedOutLinks = ()=> {
     return (
-       <ul className="right">
+       <>
+       <ul className="right hide-on-med-and-down">
            <li><NavLink to="/signup">Signup</NavLink></li>
            <li><NavLink to="/signin">Login</NavLink></li>
        </ul>
+       <ul id="nav-mobile" class="sidenav">
+       <li><NavLink to="/signup">Signup</NavLink></li>
+           <li><NavLink to="/signin">Login</NavLink></li>
+     </ul>
+     <a href="#" data-target="nav-mobile" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+       </>
     )
 }
 
